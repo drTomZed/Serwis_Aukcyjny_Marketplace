3d5db707deab15e666aeea3c02aa49bd8d8ae22c
= <tomasz.zajac403@gmail.com>
2023-03-23 17:30:37 +0100

    create spring project and GIT

---
+++ src/main/java/com/projekt_koncowy/serwis_aukcyjny_marketplace/SerwisAukcyjnyMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.projekt_koncowy.serwis_aukcyjny_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisAukcyjnyMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerwisAukcyjnyMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projekt_koncowy/serwis_aukcyjny_marketplace/SerwisAukcyjnyMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt_koncowy.serwis_aukcyjny_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisAukcyjnyMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
